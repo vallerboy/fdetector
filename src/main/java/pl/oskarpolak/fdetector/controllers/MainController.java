9e4be8b163418cb0c5ef69c455bdcc54379bd728
Oskar Polak <oskar.polak@akademiakodu.pl>
2018-11-22 09:39:32 +0100

    Add index view

---
+++ src/main/java/pl/oskarpolak/fdetector/controllers/MainController.java
@@ -0,0 +1,15 @@
+package pl.oskarpolak.fdetector.controllers;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+
+    @GetMapping("/")
+    public String index() {
+        return "index";
+    }
+
+}
