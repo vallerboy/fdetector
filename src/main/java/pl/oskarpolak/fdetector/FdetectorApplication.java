9c7a91475ba1b9e60624200d09c55bb6597e0eae
Oskar Polak <oskar.polak@akademiakodu.pl>
2018-11-22 09:13:24 +0100

    Initial commit

---
+++ src/main/java/pl/oskarpolak/fdetector/FdetectorApplication.java
@@ -0,0 +1,11 @@
+package pl.oskarpolak.fdetector;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FdetectorApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(FdetectorApplication.class, args);
+	}
+}
